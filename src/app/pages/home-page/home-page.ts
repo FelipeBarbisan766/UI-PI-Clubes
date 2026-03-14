889595f228835dfe2b94e5e79cc1ed9e01813c95
felipegalati <felipegalati20@gmail.com>
2026-03-14 15:54:45 -0300

    feat: ladind page v1 and mock data

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SearchHome } from "../../shared/components/search-home/search-home";
+import { RouterLink } from "@angular/router";
+import { NgOptimizedImage } from '@angular/common';
+
+interface Club {
+  id: number;
+  name: string;
+  location: string;
+  rating: number;
+  tags: string[];
+  price: number;
+  image: string;
+}
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [SearchHome, RouterLink, NgOptimizedImage],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage {}
+export class HomePage {
+  readonly featuredClubs = signal<Club[]>([
+    {
+      id: 1,
+      name: 'Arena Bate Bola',
+      location: 'Taquaritinga, SP',
+      rating: 4.8,
+      tags: ['Futsal', 'Vôlei', 'Beach Tennis'],
+      price: 80,
+      image: '/assets/batebola.jpg'
+    },
+    {
+      id: 2,
+      name: 'Na Praia Tênis',
+      location: 'Taquaritinga, SP',
+      rating: 4.6,
+      tags: ['Tênis', 'Beach Tennis'],
+      price: 100,
+      image: '/assets/napraia.jpg'
+    },
+    {
+      id: 3,
+      name: 'Tennis House',
+      location: 'Taquaritinga, SP',
+      rating: 4.9,
+      tags: ['Tênis'],
+      price: 120,
+      image: '/assets/tennishouse.jpg'
+    }
+  ]);
+}
--- src/app/shared/components/search-home/search-home.ts
+++ src/app/shared/components/search-home/search-home.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, signal } from '@angular/core';
+import 'cally';
 
 @Component({
   selector: 'app-search-home',
   imports: [],
   templateUrl: './search-home.html',
   styleUrl: './search-home.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class SearchHome {}
+export class SearchHome {
+  selectedDate = signal<string>('Quando?');
+
+  onDateChange(event: any) {
+    this.selectedDate.set(event.target.value);
+    
+    const popover = document.getElementById('cally-popover1');
+    if (popover) {
+      (popover as any).hidePopover(); 
+    }
+  }
+}
