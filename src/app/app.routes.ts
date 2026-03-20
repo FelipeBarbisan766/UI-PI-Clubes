5181e9b95a4421244f3d7a1446dd4b2dc5148be1
Vitor Fanelli <vitorhugofinocencio@gmail.com>
2026-03-20 19:47:11 -0300

    form-login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,30 +5,39 @@ import { MainLayout } from './layouts/main-layout/main-layout';
 import { Clubs } from './pages/clubs/clubs';
 import { AdminLayout } from './layouts/admin-layout/admin-layout';
 import { SearchHome } from './shared/components/search-home/search-home';
+import { Login } from './login/login';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
         path: '',
-        component: MainLayout,
-        children: [
-            {
-                path: '',
-                loadComponent: () => import('./pages/home-page/home-page').then((m) => HomePage)  
-            },
-            {
-                path: 'clubs',
-                loadComponent: () => import('./pages/clubs/clubs').then((m) => Clubs)
-            }
-        ]
-    },
-    {
-        path: 'admin',
-        component: AdminLayout,
-        children: [
-            {
-                path: 'teste',
-                component: SearchHome,
-            }
-        ]
-    }
+        loadComponent: () => import('./pages/home-page/home-page').then((m) => HomePage),
+      },
+      {
+        path: 'clubs',
+        loadComponent: () => import('./pages/clubs/clubs').then((m) => Clubs),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('.//login/login').then(() => Login),
+      }
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminLayout,
+    children: [
+      {
+        path: 'teste',
+        component: SearchHome,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
 ];
