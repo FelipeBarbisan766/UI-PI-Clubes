6966dcaa1b02eac804c3d52c564a57425160f4cd
felipegalati <felipegalati20@gmail.com>
2026-04-26 20:43:16 -0300

    fix api link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   name: 'production',
-  apiUrl: 'http://localhost:5000/api' // <-- A URL segura que acabamos de configurar
+  apiUrl: 'https://api.clubera.dev/api' // <-- A URL segura que acabamos de configurar
 };
