886da1da95abd0531693c66bcfe68d311f2bac24
felipebarbisan766 <felipebarbisan76@gmail.com>
2026-05-30 17:49:02 -0300

    fix: deleted mpBackUrls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,4 @@ export const environment = {
   name: 'production',
   apiUrl: 'https://api.clubera.dev/api',
   googleClientId: '612959761839-amghu5hs5c6ms3vm34qgfcbe64pak5os.apps.googleusercontent.com',
-  mpBackUrls: {
-    success: 'https://api.clubera.dev/api/payment/success',
-    failure: 'https://api.clubera.dev/api/payment/failure',
-    pending: 'https://api.clubera.dev/api/payment/pending',
-  }
 };
