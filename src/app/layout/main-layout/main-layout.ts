389cae902ad9cc0a21e43054b927ce5e5847f1e4
felipegalati <felipegalati20@gmail.com>
2026-07-07 13:27:49 -0300

    small visual changes

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from "@angular/router";
 import { Navbar } from "../../shared/navbar/navbar";
 import { Footer } from "../../shared/footer/footer";
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-main-layout',
@@ -9,4 +11,25 @@ import { Footer } from "../../shared/footer/footer";
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
-export class MainLayout {}
+export class MainLayout {
+  private router = inject(Router);
+
+  showFooter = signal<boolean>(false);
+
+  constructor() {
+    this.checkFooterVisibility(this.router.url);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      takeUntilDestroyed()
+    ).subscribe((event: NavigationEnd) => {
+      this.checkFooterVisibility(event.urlAfterRedirects);
+    });
+  }
+
+  private checkFooterVisibility(url: string): void {
+    const path = url.split('?')[0].split('#')[0];
+    
+    this.showFooter.set(path === '/' || path === '/clubs');
+  }
+}
